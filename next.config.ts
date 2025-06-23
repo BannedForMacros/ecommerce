0027fc186d725b7d493739222905cb62c23e2654
BannedForMacros <cjrmac0706@gmail.com>
2025-06-23 11:33:55 -0500

    vercel uptade2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  trailingSlash: false,
+  async rewrites() {
+    return [
+      {
+        source: '/gondolas/:path*',
+        destination: '/gondolas/:path*',
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
